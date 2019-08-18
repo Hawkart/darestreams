dd359b66cc0a84231acd5ea47cf937de94e2c043
Anton Alexeev <alexeev.sker@gmail.com>
2019-08-18 23:14:42 +0300

    Добавил установку домена в окне

--- resources/views/oauth/callback.blade.php
+++ resources/views/oauth/callback.blade.php
@@ -3,7 +3,7 @@
   <meta charset="utf-8">
   <title>{{ config('app.name') }}</title>
   <script>
-    alert(window.opener);
+    document.domain="darestreams.com";
     window.opener.postMessage({ token: "{{ $token }}", expires: "{{$expires_in}}" }, "{{  url('*',[],true)  }}");
     window.close();
   </script>
