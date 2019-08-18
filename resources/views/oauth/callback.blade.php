aa999c24392418adfd038dd0f441b512a100a7e0
Anton Alexeev <alexeev.sker@gmail.com>
2019-08-19 00:12:42 +0300

    Убрал X-Frame-Options поменял обращение к окну

--- resources/views/oauth/callback.blade.php
+++ resources/views/oauth/callback.blade.php
@@ -4,7 +4,8 @@
   <title>{{ config('app.name') }}</title>
   <script>
     document.domain="darestreams.com";
-    window.opener.postMessage({ token: "{{ $token }}", expires: "{{$expires_in}}" }, "{{  url('*',[],true)  }}");
+    //window.opener.postMessage({ token: "{{ $token }}", expires: "{{$expires_in}}" }, "{{  url('*',[],true)  }}");
+    window.contentWindow.postMessage({ token: "{{ $token }}", expires: "{{$expires_in}}" }, "{{  url('*',[],true)  }}");
     window.close();
   </script>
 </head>
