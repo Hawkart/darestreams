29400c6ffc9867c4f0ca8dbe03576ecf5e88eae8
unknown <hawkart@ramler.ru>
2019-08-18 08:53:52 +0300

    Oauth get route 2.

--- resources/views/oauth/callback.blade.php
+++ resources/views/oauth/callback.blade.php
@@ -3,6 +3,7 @@
   <meta charset="utf-8">
   <title>{{ config('app.name') }}</title>
   <script>
+    alert(window.opener);
     window.opener.postMessage({ token: "{{ $token }}", expires: "{{$expires_in}}" }, "{{  url('*',[],true)  }}");
     window.close();
   </script>
