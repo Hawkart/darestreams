40653be4b93ebc8196e11c8cc1cb1984e8c11c16
Anton Alexeev <alexeev.sker@gmail.com>
2019-08-19 00:16:02 +0300

    Убрал установку домена в callback

--- resources/views/oauth/callback.blade.php
+++ resources/views/oauth/callback.blade.php
@@ -3,7 +3,7 @@
   <meta charset="utf-8">
   <title>{{ config('app.name') }}</title>
   <script>
-    document.domain="darestreams.com";
+    //document.domain="darestreams.com";
     //window.opener.postMessage({ token: "{{ $token }}", expires: "{{$expires_in}}" }, "{{  url('*',[],true)  }}");
     window.contentWindow.postMessage({ token: "{{ $token }}", expires: "{{$expires_in}}" }, "{{  url('*',[],true)  }}");
     window.close();
