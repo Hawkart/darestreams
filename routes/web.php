ddd6c5b38991c354fb885016a5fd8c92636d04f9
unknown <hawkart@ramler.ru>
2019-08-19 09:20:02 +0300

    Deploy bugfix

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
 
-Route::post('deploy', 'DeployController@deploy');
+Route::post('deploy', 'Api\DeployController@deploy');
 
 Route::get('/', function () {
     return view('index');
