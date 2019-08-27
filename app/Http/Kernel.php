71e36bb1abbf49a20de36946db17df55b38ba84a
unknown <hawkart@ramler.ru>
2019-08-27 16:41:35 +0300

    Cors global

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\SetLocale::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -40,8 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'bindings',
-            \Barryvdh\Cors\HandleCors::class,
+            'bindings'
         ],
     ];
 
