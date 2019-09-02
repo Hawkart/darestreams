97d04999b5a6d15082ab41ff830357b986c82a61
unknown <hawkart@ramler.ru>
2019-09-02 16:14:54 +0300

    Account to me

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -62,7 +62,7 @@ class UserController extends Controller
      * Get authorized user.
      * @authenticated
      *
-     * @queryParam include string String of connections: tasks, streams, channel. Example: tasks,channel
+     * @queryParam include string String of connections: tasks, streams, channel, account. Example: tasks,channel
      *
      * @param Request $request
      * @return UserResource
@@ -70,7 +70,7 @@ class UserController extends Controller
     public function me(Request $request)
     {
         $item = QueryBuilder::for(User::class)
-            ->allowedIncludes(['tasks','streams', 'channel'])
+            ->allowedIncludes(['tasks','streams', 'channel', 'account'])
             ->findOrFail(auth()->user()->id);
 
         return new UserResource($item);
