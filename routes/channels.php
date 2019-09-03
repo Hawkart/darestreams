c70f46a01d3af60557b5454a6ff287f41b2b7878
unknown <hawkart@ramler.ru>
2019-09-03 08:27:44 +0300

    Route channel.

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,9 @@
 <?php
 
 use Cmgmyr\Messenger\Models\Thread;
+use Illuminate\Support\Facades\Log;
 
+/*
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
@@ -10,7 +12,13 @@ Broadcast::channel('channel_{thread_id}', function ($user, $thread_id) {
     $thread = Thread::findOrFail($thread_id);
     return in_array((int) $user->id, $thread->participantsUserIds());
 });
+*/
 
 Broadcast::channel('streams.{id}', function($id) {
+    Log::info('Stream socket on donate in routes', [
+        'id' => $id,
+        'file' => __FILE__,
+        'line' => __LINE__
+    ]);
     return true;
 });
