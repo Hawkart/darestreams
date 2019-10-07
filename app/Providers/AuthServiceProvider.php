dd5c027f243cf7c40e4a10eec34f02dcbb40c70d
unknown <hawkart@ramler.ru>
2019-10-07 11:47:29 +0300

    Admin login guard custom.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $this->app->singleton('VoyagerAuth', function () {
+            return Auth::guard('web');
+        });
     }
 }
