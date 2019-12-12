cb7358578555a6b130d66545fa0e900fb8fd6813
unknown <hawkart@ramler.ru>
2019-12-12 22:48:34 +0300

    Stripe add exception

--- src/Payments/Stripe.php
+++ src/Payments/Stripe.php
@@ -17,34 +17,39 @@ class Stripe implements PaymentInterface
         $user_id = $params['user_id'];
         $task_id = $params['task_id'];
 
-        $session = \Stripe\Checkout\Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [[
-                'name' => 'Donation',
-                'description' => $params['description'],
-                'amount' => $params['amount']*100,
-                'currency' => $params['currency'],
-                'quantity' => 1,
-            ]],
-            'mode' => 'payment',
-            'payment_intent_data' => [
-                'metadata' => [
-                    'order_id' => $params['transactionId'],
-                    'user_id' => $user_id,
-                    'task_id' => $task_id
-                ]
-            ],
-            'success_url' => route('payment.checkout.completed', ['gateway' => 'stripe', 'user' => $user_id, 'task' => $task_id]),
-            'cancel_url' => route('payment.checkout.cancelled', ['gateway' => 'paypal', 'user' => $user_id, 'task' => $task_id]),
-        ]);
+        try {
+            $session = \Stripe\Checkout\Session::create([
+                'payment_method_types' => ['card'],
+                'line_items' => [[
+                    'name' => 'Donation',
+                    'description' => $params['description'],
+                    'amount' => $params['amount']*100,
+                    'currency' => $params['currency'],
+                    'quantity' => 1,
+                ]],
+                'mode' => 'payment',
+                'payment_intent_data' => [
+                    'metadata' => [
+                        'order_id' => $params['transactionId'],
+                        'user_id' => $user_id,
+                        'task_id' => $task_id
+                    ]
+                ],
+                'success_url' => route('payment.checkout.completed', ['gateway' => 'stripe', 'user' => $user_id, 'task' => $task_id]),
+                'cancel_url' => route('payment.checkout.cancelled', ['gateway' => 'paypal', 'user' => $user_id, 'task' => $task_id]),
+            ]);
 
-        if($session)
-            $params['result']->update(['exid' => $session['id']]);
+            if($session)
+                $params['result']->update(['exid' => $session['id']]);
 
-        return response()->view('payments.stripe', [
-            'session_id' => $session['id'],
-            'key' => config('services.stripe.key')
-        ], 200);
+            return response()->view('payments.stripe', [
+                'session_id' => $session['id'],
+                'key' => config('services.stripe.key')
+            ], 200);
+            
+        } catch (\Exception $exception) {
+            return abort(404, $exception->getMessage());
+        }
     }
 
     public function completed(Request $request)
